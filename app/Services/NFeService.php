4a73d0af3aac84de7e1dd89211c98643e4cf30c5
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-12 08:52:22 -0300

    Upload initial files and NFeService Class

---
+++ app/Services/NFeService.php
@@ -0,0 +1,454 @@
+<?php
+
+
+namespace App\Services;
+
+use NFePHP\NFe\Make;
+
+/**
+ * Class NFeService
+ * @package App\Services
+ */
+class NFeService
+{
+    protected $configJson;
+
+    /**
+     * NFeService constructor.
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        if($config) {
+            $this->configJson = json_encode($config);
+        } else {
+            $configDefaul = [
+                "atualizacao" => "2015-10-02 06:01:21",
+                "tpAmb" => 2,
+                "razaosocial" => "Fake Materiais de construção Ltda",
+                "siglaUF" => "SP",
+                "cnpj" => "00716345000119",
+                "schemes" => "PL_008i2",
+                "versao" => "3.10",
+                "tokenIBPT" => "AAAAAAA",
+                "CSC" => "GPB0JBWLUR6HWFTVEAS6RJ69GPCROFPBBB8G",
+                "CSCid" => "000002",
+                "aProxyConf" => [
+                    "proxyIp" => "",
+                    "proxyPort" => "",
+                    "proxyUser" => "",
+                    "proxyPass" => ""
+                ]
+            ];
+
+            $this->configJson = json_encode($configDefaul);
+
+        }
+
+
+    }
+
+    public function gerarNFe(StdClass $nfe)
+    {
+        $nfe = new Make();
+
+        $stdPrincipal = new \stdClass();
+        $stdPrincipal->versao = '4.00'; //versão do layout (string)
+        //$stdPrincipal->Id = 'NFe35150271780456000160550010000000021800700082'; //se o Id de 44 digitos não for passado será gerado automaticamente
+        $stdPrincipal->pk_nItem = null;//deixe essa variavel sempre como NULL
+
+        $nfe->taginfNFe($stdPrincipal);
+
+        $stdIdeNfe = new stdClass();
+        $stdIdeNfe->cUF = 43;
+        $stdIdeNfe->cNF = '01382139';
+        $stdIdeNfe->natOp = '5.405 - Venda de Mercadoria TESTE SPED-NFE PHP';
+        $stdIdeNfe->indPag = 0; //NÃO EXISTE MAIS NA VERSÃO 4.00
+        $stdIdeNfe->mod = 55;
+        $stdIdeNfe->serie = 1;
+        $stdIdeNfe->nNF = 167;
+        $stdIdeNfe->dhEmi = self::getDateIso();
+        $stdIdeNfe->dhSaiEnt = self::getDateIso();
+        $stdIdeNfe->tpNF = 1;
+        $stdIdeNfe->idDest = 1;
+        $stdIdeNfe->cMunFG = 4305108;
+        $stdIdeNfe->tpImp = 1;
+        $stdIdeNfe->tpEmis = 1;
+        $stdIdeNfe->cDV = 2;
+        $stdIdeNfe->tpAmb = 2;
+        $stdIdeNfe->finNFe = 1;
+        $stdIdeNfe->indFinal = 0;
+        $stdIdeNfe->indPres = 1;
+        $stdIdeNfe->indIntermed = null; //usar a partir de 05/04/2021
+        $stdIdeNfe->procEmi = 0;
+        $stdIdeNfe->verProc = 'MILL_4.1000.0.83';
+        $stdIdeNfe->dhCont = null;
+        $stdIdeNfe->xJust = null;
+
+        $nfe->tagide($stdIdeNfe);
+
+        $stdEmitente = new stdClass();
+        $stdEmitente->xNome = 'MILLENNIUM Sistemas de Gestao - Posto Autorizado Premium';
+        $stdEmitente->xFant = 'MILLMOTORS';
+        $stdEmitente->IE = '0290419603';
+        $stdEmitente->IEST = null;
+        $stdEmitente->IM = '83067';
+        $stdEmitente->CNAE = null;
+        $stdEmitente->CRT = 1;
+        $stdEmitente->CNPJ = '06103611000141'; //indicar apenas um CNPJ ou CPF
+        $stdEmitente->CPF = null;
+
+        $nfe->tagemit($stdEmitente);
+
+        $stdEnderecoEmitente = new stdClass();
+        $stdEnderecoEmitente->xLgr   = "AVENIDA RIO BRANCO TESTE";
+        $stdEnderecoEmitente->nro    = "1512";
+        $stdEnderecoEmitente->xCpl   = "SALA 02";
+        $stdEnderecoEmitente->xBairro= "RIO BRANCO";
+        $stdEnderecoEmitente->cMun   = 4305108;
+        $stdEnderecoEmitente->xMun   = "CAXIAS DO SUL";
+        $stdEnderecoEmitente->UF     = "RS";
+        $stdEnderecoEmitente->CEP    = "95096000";
+        $stdEnderecoEmitente->cPais  = 1058;
+        $stdEnderecoEmitente->xPais  = "Brasil";
+        $stdEnderecoEmitente->fone   = "5430252422";
+
+        $nfe->tagenderEmit($stdEnderecoEmitente);
+
+        $stdDestinatário = new stdClass();
+        $stdDestinatário->xNome         = "NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL";
+        $stdDestinatário->indIEDest     = 2;
+        $stdDestinatário->IE            = null;
+        $stdDestinatário->ISUF          = null;
+        $stdDestinatário->IM            = null;
+        $stdDestinatário->email         = null;
+        $stdDestinatário->CNPJ          = null; //indicar apenas um CNPJ ou CPF ou idEstrangeiro
+        $stdDestinatário->CPF           = "64831124087";
+        $stdDestinatário->idEstrangeiro = null;
+
+        $nfe->tagdest($stdDestinatário);
+
+        $stdEnderecoDestinatario = new stdClass();
+        $stdEnderecoDestinatario->xLgr    = "Garibaldi";
+        $stdEnderecoDestinatario->nro     = "803";
+        $stdEnderecoDestinatario->xCpl    = null;
+        $stdEnderecoDestinatario->xBairro = "Centro";
+        $stdEnderecoDestinatario->cMun    = 4305108;
+        $stdEnderecoDestinatario->xMun    = "CAXIAS DO SUL";
+        $stdEnderecoDestinatario->UF      = "RS";
+        $stdEnderecoDestinatario->CEP     = "95012270";
+        $stdEnderecoDestinatario->cPais   = 1058;
+        $stdEnderecoDestinatario->xPais   = "Brasil";
+        $stdEnderecoDestinatario->fone    = "0543027179";
+
+        $nfe->tagenderDest($stdEnderecoDestinatario);
+
+        $stdAutorizadoXml = new stdClass();
+        $stdAutorizadoXml->CNPJ = null; //indicar um CNPJ ou CPF
+        $stdAutorizadoXml->CPF = '00132986078';
+
+        $nfe->tagautXML($stdAutorizadoXml);
+
+        $stdProdutoItem = new stdClass();
+        $stdProdutoItem->item     = 1; //item da NFe
+        $stdProdutoItem->cProd    = 3114;
+        $stdProdutoItem->cEAN     = "SEM GTIN";
+        $stdProdutoItem->xProd    = "FAGNER - MOTOR COMPRESSOR - MFS - RHF5 - 44,93 / 58mm - 6+6 Palhetas";
+        $stdProdutoItem->NCM      = "84149039";
+        $stdProdutoItem->cBenef   = null; //incluido no layout 4.00
+        $stdProdutoItem->EXTIPI   = "000";
+        $stdProdutoItem->CFOP     = 5405;
+        $stdProdutoItem->uCom     = "UN";
+        $stdProdutoItem->qCom     = 3;
+        $stdProdutoItem->vUnCom   = 69.00;
+        $stdProdutoItem->cEANTrib = "SEM GTIN";
+        $stdProdutoItem->uTrib    = "UN";
+        $stdProdutoItem->qTrib    = 3.00;
+        $stdProdutoItem->vUnTrib  = 69.00;
+        $stdProdutoItem->vProd    = (double) $stdProdutoItem->qTrib * $stdProdutoItem->vUnTrib;
+        $stdProdutoItem->vFrete   = 0.00;
+        $stdProdutoItem->vSeg     = 0.00;
+        $stdProdutoItem->vDesc    = 0.00;
+        $stdProdutoItem->vOutro   = 0.00;
+        $stdProdutoItem->indTot   = 1;
+        $stdProdutoItem->xPed     = null;
+        $stdProdutoItem->nItemPed = null;
+        $stdProdutoItem->nFCI     = null;
+
+        $nfe->tagprod($stdProdutoItem);
+
+
+        $stdEspecificacaoST = new stdClass();
+        $stdEspecificacaoST->item      = 1; //item da NFe
+        $stdEspecificacaoST->CEST      = '0103500';
+        $stdEspecificacaoST->indEscala = 'N'; //incluido no layout 4.00
+        $stdEspecificacaoST->CNPJFab   = '12345678901234'; //incluido no layout 4.00
+
+        $nfe->tagCEST($stdEspecificacaoST);
+
+        $stdImpostoItem = new stdClass();
+        $stdImpostoItem->item     = 1; //item da NFe
+        $stdImpostoItem->vTotTrib = 39.16;
+
+        $nfe->tagimposto($stdImpostoItem);
+
+//        $stdICMSItem = new stdClass();
+//        $stdICMSItem->item            = 1; //item da NFe
+//        $stdICMSItem->orig            = 1;
+//        $stdICMSItem->CST             = "";
+//        $stdICMSItem->modBC           = "";
+//        $stdICMSItem->vBC             = "";
+//        $stdICMSItem->pICMS           = "";
+//        $stdICMSItem->vICMS           = "";
+//        $stdICMSItem->pFCP            = "";
+//        $stdICMSItem->vFCP            = "";
+//        $stdICMSItem->vBCFCP          = "";
+//        $stdICMSItem->modBCST         = "";
+//        $stdICMSItem->pMVAST          = "";
+//        $stdICMSItem->pRedBCST        = "";
+//        $stdICMSItem->vBCST           = "";
+//        $stdICMSItem->pICMSST         = "";
+//        $stdICMSItem->vICMSST         = "";
+//        $stdICMSItem->vBCFCPST        = "";
+//        $stdICMSItem->pFCPST          = "";
+//        $stdICMSItem->vFCPST          = "";
+//        $stdICMSItem->vICMSDeson      = "";
+//        $stdICMSItem->motDesICMS      = "";
+//        $stdICMSItem->pRedBC          = "";
+//        $stdICMSItem->vICMSOp         = "";
+//        $stdICMSItem->pDif            = "";
+//        $stdICMSItem->vICMSDif        = "";
+//        $stdICMSItem->vBCSTRet        = "";
+//        $stdICMSItem->pST             = "";
+//        $stdICMSItem->vICMSSTRet      = "";
+//        $stdICMSItem->vBCFCPSTRet     = "";
+//        $stdICMSItem->pFCPSTRet       = "";
+//        $stdICMSItem->vFCPSTRet       = "";
+//        $stdICMSItem->pRedBCEfet      = "";
+//        $stdICMSItem->vBCEfet         = "";
+//        $stdICMSItem->pICMSEfet       = "";
+//        $stdICMSItem->vICMSEfet       = "";
+//        $stdICMSItem->vICMSSubstituto = ""; //NT2018.005_1.10_Fevereiro de 2019
+//
+//        $nfe->tagICMS($stdICMSItem);
+
+        $stdICMSSTRet = new stdClass();
+        $stdICMSSTRet->item = 1; //item da NFe
+        $stdICMSSTRet->orig = 0;
+        $stdICMSSTRet->CST = '60';
+        $stdICMSSTRet->vBCSTRet = 2.49;
+        $stdICMSSTRet->vICMSSTRet = 0.42;
+        $stdICMSSTRet->vBCSTDest = null;
+        $stdICMSSTRet->vICMSSTDest = null;
+        $stdICMSSTRet->vBCFCPSTRet = null;
+        $stdICMSSTRet->pFCPSTRet = null;
+        $stdICMSSTRet->vFCPSTRet = null;
+        $stdICMSSTRet->pST = null;
+        $stdICMSSTRet->vICMSSubstituto = null;
+        $stdICMSSTRet->pRedBCEfet = null;
+        $stdICMSSTRet->vBCEfet = null;
+        $stdICMSSTRet->pICMSEfet = null;
+        $stdICMSSTRet->vICMSEfet = null;
+
+        $nfe->tagICMSST($stdICMSSTRet);
+
+        $stdICMSSNItem = new stdClass();
+        $stdICMSSNItem->item = 1; //item da NFe
+        $stdICMSSNItem->orig = 1;
+        $stdICMSSNItem->CSOSN = '500';
+        $stdICMSSNItem->pCredSN = 2.00;
+        $stdICMSSNItem->vCredICMSSN = 20.00;
+        $stdICMSSNItem->modBCST = null;
+        $stdICMSSNItem->pMVAST = null;
+        $stdICMSSNItem->pRedBCST = null;
+        $stdICMSSNItem->vBCST = null;
+        $stdICMSSNItem->pICMSST = null;
+        $stdICMSSNItem->vICMSST = null;
+        $stdICMSSNItem->vBCFCPST = null; //incluso no layout 4.00
+        $stdICMSSNItem->pFCPST = null; //incluso no layout 4.00
+        $stdICMSSNItem->vFCPST = null; //incluso no layout 4.00
+        $stdICMSSNItem->vBCSTRet = null;
+        $stdICMSSNItem->pST = null;
+        $stdICMSSNItem->vICMSSTRet = null;
+        $stdICMSSNItem->vBCFCPSTRet = null; //incluso no layout 4.00
+        $stdICMSSNItem->pFCPSTRet = null; //incluso no layout 4.00
+        $stdICMSSNItem->vFCPSTRet = null; //incluso no layout 4.00
+        $stdICMSSNItem->modBC = null;
+        $stdICMSSNItem->vBC = null;
+        $stdICMSSNItem->pRedBC = null;
+        $stdICMSSNItem->pICMS = null;
+        $stdICMSSNItem->vICMS = null;
+        $stdICMSSNItem->pRedBCEfet = null;
+        $stdICMSSNItem->vBCEfet = null;
+        $stdICMSSNItem->pICMSEfet = null;
+        $stdICMSSNItem->vICMSEfet = null;
+        $stdICMSSNItem->vICMSSubstituto = null;
+
+        $nfe->tagICMSSN($stdICMSSNItem);
+
+        $stdIPIItem = new stdClass();
+        $stdIPIItem->item = 1; //item da NFe
+        $stdIPIItem->clEnq = null;
+        $stdIPIItem->CNPJProd = null;
+        $stdIPIItem->cSelo = null;
+        $stdIPIItem->qSelo = null;
+        $stdIPIItem->cEnq = '999';
+        $stdIPIItem->CST = '99';
+        $stdIPIItem->vIPI = 150.00;
+        $stdIPIItem->vBC = 1000.00;
+        $stdIPIItem->pIPI = 15.00;
+        $stdIPIItem->qUnid = 0.00;
+        $stdIPIItem->vUnid = 0.00;
+
+        $nfe->tagIPI($stdIPIItem);
+
+        $stdPISItem = new stdClass();
+        $stdPISItem->item = 1; //item da NFe
+        $stdPISItem->CST = '04';
+        $stdPISItem->vBC = null;
+        $stdPISItem->pPIS = null;
+        $stdPISItem->vPIS = null;
+        $stdPISItem->qBCProd = null;
+        $stdPISItem->vAliqProd = null;
+
+        $nfe->tagPIS($stdPISItem);
+
+        $stdCOFINSItem = new stdClass();
+        $stdCOFINSItem->item = 1; //item da NFe
+        $stdCOFINSItem->CST = '04';
+        $stdCOFINSItem->vBC = null;
+        $stdCOFINSItem->pCOFINS = null;
+        $stdCOFINSItem->vCOFINS = null;
+        $stdCOFINSItem->qBCProd = null;
+        $stdCOFINSItem->vAliqProd = null;
+
+        $nfe->tagCOFINS($stdCOFINSItem);
+
+        $stdTotaisICMSItem = new stdClass();
+        $stdTotaisICMSItem->vBC = 1000.00;
+        $stdTotaisICMSItem->vICMS = 1000.00;
+        $stdTotaisICMSItem->vICMSDeson = 1000.00;
+        $stdTotaisICMSItem->vFCP = 1000.00; //incluso no layout 4.00
+        $stdTotaisICMSItem->vBCST = 1000.00;
+        $stdTotaisICMSItem->vST = 1000.00;
+        $stdTotaisICMSItem->vFCPST = 1000.00; //incluso no layout 4.00
+        $stdTotaisICMSItem->vFCPSTRet = 1000.00; //incluso no layout 4.00
+        $stdTotaisICMSItem->vProd = 1000.00;
+        $stdTotaisICMSItem->vFrete = 1000.00;
+        $stdTotaisICMSItem->vSeg = 1000.00;
+        $stdTotaisICMSItem->vDesc = 1000.00;
+        $stdTotaisICMSItem->vII = 1000.00;
+        $stdTotaisICMSItem->vIPI = 1000.00;
+        $stdTotaisICMSItem->vIPIDevol = 1000.00; //incluso no layout 4.00
+        $stdTotaisICMSItem->vPIS = 1000.00;
+        $stdTotaisICMSItem->vCOFINS = 1000.00;
+        $stdTotaisICMSItem->vOutro = 1000.00;
+        $stdTotaisICMSItem->vNF = 1000.00;
+        $stdTotaisICMSItem->vTotTrib = 1000.00;
+
+        $nfe->tagICMSTot($stdTotaisICMSItem);
+
+        $stdFrete = new stdClass();
+        $stdFrete->modFrete = 1;
+
+        $nfe->tagtransp($stdFrete);
+
+        $stdTransportadora = new stdClass();
+        $stdTransportadora->xNome = 'Rodo Fulano';
+        $stdTransportadora->IE = '12345678901';
+        $stdTransportadora->xEnder = 'Rua Um, sem numero';
+        $stdTransportadora->xMun = 'Cotia';
+        $stdTransportadora->UF = 'SP';
+        $stdTransportadora->CNPJ = '12345678901234';//só pode haver um ou CNPJ ou CPF, se um deles é especificado o outro deverá ser null
+        $stdTransportadora->CPF = null;
+
+        $nfe->tagtransporta($stdTransportadora);
+
+        $stdVeiculoTrator = new stdClass();
+        $stdVeiculoTrator->placa = 'ABC1111';
+        $stdVeiculoTrator->UF = 'RJ';
+        $stdVeiculoTrator->RNTC = '999999';
+
+        $nfe->tagveicTransp($stdVeiculoTrator);
+
+        $stdReboque = new stdClass();
+        $stdReboque->placa = 'BCB0897';
+        $stdReboque->UF = 'SP';
+        $stdReboque->RNTC = '123456';
+
+        $nfe->tagreboque($stdReboque);
+
+        $stdVolumes = new stdClass();
+        $stdVolumes->item = 1; //indicativo do numero do volume
+        $stdVolumes->qVol = 2;
+        $stdVolumes->esp = 'caixa';
+        $stdVolumes->marca = 'OLX';
+        $stdVolumes->nVol = '11111';
+        $stdVolumes->pesoL = 10.50;
+        $stdVolumes->pesoB = 11.00;
+
+        $nfe->tagvol($stdVolumes);
+
+        $stdFaturaCobranca = new stdClass();
+        $stdFaturaCobranca->nFat = '1233';
+        $stdFaturaCobranca->vOrig = 1254.22;
+        $stdFaturaCobranca->vDesc = null;
+        $stdFaturaCobranca->vLiq = 1254.22;
+
+        $nfe->tagfat($stdFaturaCobranca);
+
+        $stdDuplicata = new stdClass();
+        $stdDuplicata->nDup = '1233-1';
+        $stdDuplicata->dVenc = '2017-08-22';
+        $stdDuplicata->vDup = 1254.22;
+
+        $nfe->tagdup($stdDuplicata);
+
+        $stdFormaPgto = new stdClass();
+        $stdFormaPgto->tPag = '03';
+        $stdFormaPgto->vPag = 200.00; //Obs: deve ser informado o valor pago pelo cliente
+        $stdFormaPgto->CNPJ = '12345678901234';
+        $stdFormaPgto->tBand = '01';
+        $stdFormaPgto->cAut = '3333333';
+        $stdFormaPgto->tpIntegra = 1; //incluso na NT 2015/002
+        $stdFormaPgto->indPag = '0'; //0= Pagamento à Vista 1= Pagamento à Prazo
+
+        $nfe->tagdetPag($stdFormaPgto);
+
+        $stdInfoAdic = new stdClass();
+        $stdInfoAdic->infAdFisco = 'informacoes para o fisco';
+        $stdInfoAdic->infCpl = 'informacoes complementares';
+
+        $nfe->taginfAdic($stdInfoAdic);
+
+        $xml = $nfe->montaNFe();
+
+        $erros = $nfe->getErrors();
+
+        echo json_encode($nfe);
+
+
+    }
+
+    public static function getDateIso()
+    {
+        $fuso = new DateTimeZone('America/Sao_Paulo');
+        $date = new DateTime();
+        $date->setTimezone($fuso);
+
+        return $date->format(Datetime::ATOM);
+    }
+
+    public function jsonSerialize() {
+        //$vars = array_merge(get_object_vars($this),parent::jsonSerialize());
+        //return $vars;
+        return get_object_vars($this);
+    }
+
+
+
+
+
+
+
+}
