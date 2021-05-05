1d1d319f163c003acc84eb7900585e3c1c4348fd
Fagner Lopes <fagnernlopes@gmail.com>
2021-05-05 11:34:57 -0300

    Create Cidades and Estados Models

---
+++ app/Models/Cidade.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cidade extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'estado_id',
+        'nome',
+        'codigo_ibge',
+    ];
+
+    public function estado(){
+        return $this->belongsTo(Estado::class);
+    }
+
+}
---
+++ app/Models/Estado.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estado extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    public function cidades(){
+        return $this->hasMany(Cidade::class);
+    }
+}
